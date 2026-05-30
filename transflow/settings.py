3cebfb19e6d532a09edbf2ea403cb141a36707c2
AJIL ESTREVILLO <ssanchez3@ssct.edu.ph>
2026-05-30 16:26:20 +0800

    fix CSRF trusted origins

--- transflow/settings.py
+++ transflow/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-transflow-secret-key-change-this-in-production'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
